76c15b642c9aca0a97eea5315b82a8cc6719d05f
Svetlana Ušakova <svusak@ttu.ee>
2019-05-21 20:10:22 +0300

    lisatut saatmine TCP pordi kaudu

--- python/dataConverter.py
+++ python/dataConverter.py
@@ -19,20 +19,27 @@ def run(command):
 
 
 def generatePackets():
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = socket.gethostname()
+    port = 9999
+    s.connect((host, port))
+
     f = open("file.txt", "w+")
     packages = []
 
     hexData = ""
 
     for line in run("kissutil -v"):
-        
+
         if line.startswith(" "):
             hexData += line[8:55].replace(' ', '');
-            
+
         if line.startswith("[0]"):
             data = hexData[4:-2]
             packages.append(data)
             f.write(data + '\n')
+            s.send(data)
             print(data)
             hexData = ""
 
